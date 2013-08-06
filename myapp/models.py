8e30de93dea1908ae363b8556e3f89d36b031e79
Collin Anderson <collin@onetencommunications.com>
2013-08-05 23:07:07 -0400

    limit_choices_to with raw_id_fields and callable

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,6 @@
+import datetime
 from django.db import models
 
 # Create your models here.
+class MyModel(models.Model):
+    user = models.ForeignKey('auth.User', limit_choices_to={'last_login__lte': datetime.date.today})
